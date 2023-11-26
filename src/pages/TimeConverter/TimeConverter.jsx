2e60579e7613c895ae6f206051388057ddbab80f
DeskorED <rfpfywtdf29@gmail.com>
2023-11-26 13:45:02 -0500

    refactor:     modify: TimeConverter.jsx

--- src/pages/TimeConverter/TimeConverter.jsx
+++ src/pages/TimeConverter/TimeConverter.jsx
@@ -20,22 +20,36 @@ export const TimeConverter = () => {
             {
                 timeLines?.map(
                     (timeLine) => {
+
+                        let currentMinutes = currentTime?.minutes + timeLine?.value?.minutes;
+                        let extraHour = currentMinutes >= 60 ? 1 : 0;
+                        let currentHoures = currentTime?.hours + timeLine?.value?.hours + extraHour;
+
                         let currentTimeLine = {
-                            hours: currentTime?.hours + timeLine?.value?.hours,
-                            minutes: currentTime?.minutes + timeLine?.value?.minutes,
+                            minutes: currentMinutes >= 60 ? currentMinutes % 60 : currentMinutes,
+                            hours: currentHoures >= 24 ? currentHoures % 24 : currentHoures
                         }
 
                         return (
-                            <div className="time-line">
+                            <div className="time-line" key={timeLine?.name}>
                                 <input type="time" id={timeLine?.name}
                                     value={getValidateTime(currentTimeLine)}
                                     onChange={
                                         (event) => {
                                             console.log(event.target.value)
+                                            let currentChangeValue = {
+                                                hours: event.target.value.substring(0, 2),
+                                                minutes: event.target.value.substring(3)
+                                            }
+
+                                            let currentMinutes = currentChangeValue?.minutes - timeLine?.value?.minutes;
+                                            let extraHour = currentMinutes < 0 ? -1 : 0;
+                                            let currentHoures = currentChangeValue?.hours - timeLine?.value?.hours + extraHour;
+
                                             setCurrentTime(
                                                 {
-                                                    hours: +event?.target?.value?.substring(0, 2) - timeLine?.value?.hours,
-                                                    minutes: +event?.target?.value?.substring(3) - timeLine?.value?.minutes
+                                                    minutes: currentMinutes < 0 ? 60 + currentMinutes : currentMinutes,
+                                                    hours: currentHoures < 0 ? 24 + currentHoures : currentHoures
                                                 }
                                             )
                                         }
